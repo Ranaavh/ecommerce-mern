8b1586934666dd93d27094d169794cdd64044886
RANA <ranavh003@gmail.com>
2024-08-13 21:03:55 +0530

    updated authmiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 
+// Middleware to check if user is authenticated,Protect certain routes from unauthorized access.
 const authMiddleware = (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
 
@@ -15,7 +16,7 @@ const authMiddleware = (req, res, next) => {
     next(); // Proceed to the next middleware/route handler
   } catch (error) {
     console.error("Auth Middleware Error:", error);
-    res.status(400).json({ message: "Invalid token." });
+    res.status(400).json({ message: "Invalid token or expired." }); // token invalid/expired
   }
 };
 
